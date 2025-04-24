69a61198288d0e57c36e7838d4dd9bf439445abf
Omar <omarnabil348@gmail.com>
2025-04-25 01:14:40 +0300

    feat: restructure app layout by replacing home component with header and education sections, update styles and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EducationComponent } from './components/education/education.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HomeComponent {}
+export class HeaderComponent {}
